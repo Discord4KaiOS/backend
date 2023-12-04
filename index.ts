40ffcf60d437067b22634d6e53fd0805dc5c0ca9
cyan-2048 <72785466+cyan-2048@users.noreply.github.com>
2023-12-04 17:18:12 +0800

    useful exports

--- index.ts
+++ index.ts
@@ -3,3 +3,5 @@ export * from "./src/DiscordGuild.ts";
 export { default as DiscordClient } from "./src/DiscordClient.ts";
 export { MFA } from "./src/setup.ts";
 export { default as setup } from "./src/setup.ts";
+export { WritableStore } from "./src/lib/utils.ts";
+export * from "./src/DiscordChannels.ts";
