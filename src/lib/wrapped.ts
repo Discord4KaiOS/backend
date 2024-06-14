c3d7752b06f470b4aeebcd84cbb058e1d5d14e3c
cyan2048 <cyankindasus@protonmail.com>
2024-06-14 12:41:41 +0800

    fucking hell

--- src/lib/wrapped.ts
+++ src/lib/wrapped.ts
@@ -1,7 +1,9 @@
 import * as Comlink from "comlink";
 import type { Exposed } from "./worker";
 
-const worker = new Worker(new URL("./worker.ts", import.meta.url));
+const worker = new Worker(new URL("./worker.ts", import.meta.url), {
+	type: "module",
+});
 
 const wrapped = Comlink.wrap<Exposed>(worker);
 
